e848287605e5e9f9800cae0825c0b2bde9beeb85
RIOterN <1274368573@qq.com>
2021-08-26 15:51:36 +0800

    123

--- Cpp_Multiple/chapter4/main/main.cpp
+++
@@ -1,8 +0,0 @@
-#include <iostream>
-using namespace std;
-#include "test.h"
-int main(){
-    fun();
-    cout << "你好"<<endl;
-    system("pause");
-}
--- Cpp_Multiple/chapter4/main/test.cpp
+++
@@ -1,5 +0,0 @@
-#include "test.h"
-#include <iostream>
-void fun(){
-    std::cout<<"你好"<<std::endl;
-}
